9c6b85bc8c6691058d39d641a6c379c20f9e4ce6
Antoine Bellemare <antoine.bellemare9@gmail.com>
2019-12-09 21:55:26 -0500

    Added option for sequence of images

--- generate_gif.py
+++ generate_gif.py
@@ -9,11 +9,12 @@ import PIL
 from PIL import Image
 import scipy.misc
 import imageio
+import itertools
 
 def generate_perlin_noise_2d(shape, res):
     def f(t):
         return 6*t**5 - 15*t**4 + 10*t**3
-
+    
     delta = (res[0] / shape[0], res[1] / shape[1])
     d = (shape[0] // res[0], shape[1] // res[1])
     grid = np.mgrid[0:res[0]:delta[0],0:res[1]:delta[1]].transpose(1, 2, 0) % 1
@@ -34,7 +35,7 @@ def generate_perlin_noise_2d(shape, res):
     n0 = n00*(1-t[:,:,0]) + t[:,:,0]*n10
     n1 = n01*(1-t[:,:,0]) + t[:,:,0]*n11
     return np.sqrt(2)*((1-t[:,:,1])*n0 + t[:,:,1]*n1)
-
+        
 def generate_fractal_noise_2d(shape, res, octaves=1, persistence=0.5):
     noise = np.zeros(shape)
     frequency = 1
@@ -46,39 +47,80 @@ def generate_fractal_noise_2d(shape, res, octaves=1, persistence=0.5):
     return noise
 
 
+
 def fractal_generator(persistence):
-    perlin = generate_fractal_noise_2d((256*4,256*4), (2,2), octaves=8, persistence=persistence)
-    img = Image.fromarray(perlin)
-    np.median(perlin)
-    for i in range(len(perlin)):
-        for j in range(len(perlin)):
-            if perlin[i][j] >= 0:
-                perlin[i][j] = 1
-            elif perlin[i][j] < 0:
-                perlin[i][j] = -1
+    perlin = generate_fractal_noise_2d((256,256), (2,2), octaves=8, persistence=persistence)
+    #perlin = (perlin)/np.max(np.max(abs(perlin)))
+    #for i in range(len(perlin)):
+    #    for j in range(len(perlin)):
+    #        if perlin[i][j] >= 0:
+    #            perlin[i][j] = 1
+    #        elif perlin[i][j] < 0:
+    #            perlin[i][j] = -1
     return perlin
 
-def increments (steps):
+def increments (steps, direction = 0):
     steps = int(100/steps)
     a = range(0,100,steps)
     b = [x for x in a]
-    c = b + [100]
-    b.reverse()
-    d = c + b
+    
+    if direction == 1:
+        c = b + [100]
+        b.reverse()
+        d = c + b
+    else:
+        c = b
+        d = c
     e = [x/100 for x in d]
     return e
 
-def FractalMorphing(imageA, imageB, steps):
+def Interpol(imageA, imageB, steps, direction = 0):
     inter_matrix = []
-    steps = increments(steps)
-    imageA = Image.fromarray(imageA).convert('L')
-    imageB = Image.fromarray(imageB).convert('L')
+    steps = increments(steps, direction)
+    imageA = Image.fromarray(imageA).convert('L') 
+    imageB = Image.fromarray(imageB).convert('L') 
     for k in steps:
         PIL.Image.blend(imageA, imageB, k)
         outImage = np.asarray(imageA) * (1.0 - k) + np.asarray(imageB) * k
         inter_matrix.append(outImage)
-        final_matrix = np.array(inter_matrix).T
-    return final_matrix
+        #final_matrix = np.array(inter_matrix).T
+    return inter_matrix
+
+def contrast(image):    
+    for i in range(len(image)):
+        for j in range(len(image)):
+            if image[i][j] >= 0:
+                image[i][j] = 1
+            elif image[i][j] < 0:
+                image[i][j] = -1
+    return image
+
+def InterpolMulti(size, list_image, steps, direction = 0):
+    size = 256
+    InterpolTemp = np.empty((0, size, size))
+
+    for first, second in zip(list_image, list_image[1:]):
+        InterpolMatrix = Interpol(first, second, steps, direction)
+        InterpolMatrix = np.array(InterpolMatrix)
+        InterpolTemp = np.vstack((InterpolTemp, InterpolMatrix))
+    InterpolMulti = np.array(InterpolTemp)
+    return InterpolMulti
+
+def Generate_list_images(list_persistence):
+    list_temp = []
+    for x in list_persistence:
+        image = contrast(fractal_generator(x))
+        list_temp.append(image)
+    list_image = np.array(list_temp)
+    return list_image
+
+def create_anim(img_arr):
+    img = []
+    for i in range(img_arr.shape[2]):
+        im = plt.imshow(img_arr[:,:,i], animated=True, cmap='gray')
+        img.append([im])
+    return img
+
 
 
 def create_anim(img_arr):
@@ -104,11 +146,10 @@ if __name__ == '__main__':
     #plt.show()
 
 
-    image1 = fractal_generator(0.7)
-    image2 = fractal_generator(0.8)
-    InterpolMatrix = FractalMorphing(image1, image2, 50)
-
-    img_arr = InterpolMatrix
+    list_persistence = [0.1, 0.2, 0.3, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90]
+    list_image = Generate_list_images(list_persistence)
+    Interpol = InterpolMulti(256, list_image, 20, direction = 0)
+    img_arr = Interpol
 
 
     fig = plt.figure()
