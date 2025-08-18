108e9d178c31bbdf55e6a2278b25af0b10834a52
AntoineBellemare <antoine.bellemare9@gmail.com>
2025-08-17 22:29:02 -0400

    restructure

---
+++ old-dev/fractal_generator.py
@@ -0,0 +1,153 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams['animation.ffmpeg_path'] = '/usr/bin/ffmpeg'
+import PIL
+from PIL import Image
+import scipy.misc
+import imageio
+import itertools
+import glob
+import cv2
+from threading import Thread, RLock
+import random
+from utils import contrast, Interpol, fractal_generator, generate_rgb_img
+from pythonosc import dispatcher, osc_server
+import sys
+import time
+from pythonosc.osc_server import AsyncIOOSCUDPServer
+from pythonosc.dispatcher import Dispatcher
+import asyncio
+from delaunay_triangulation import delaunay_morphing
+
+
+class Generator(Thread):
+    '''
+    Generates a sequence of images that interpolates between a base image and a new created fractal
+    This thread updates the next_imgs list when to_compute is set to True by the Display thread
+    '''
+    def __init__(self, interp_steps):
+        Thread.__init__(self)
+        self.interp_steps = interp_steps
+
+    def run(self):
+        global to_compute, next_imgs, FD_control
+        while True:
+            if to_compute==True:
+                if 'last_image' not in locals():
+                    last_image_temp1 = fractal_generator(0.1, size=size)
+                    last_image_temp2 = fractal_generator(0.1, size=size)
+                    last_image_temp3 = fractal_generator(0.1, size=size)
+                    last_image = generate_rgb_img(last_image_temp1, last_image_temp2, last_image_temp3)
+
+                to_compute = False
+                FD_control = np.random.random()
+                start = time.time()
+                end_image_temp1 = fractal_generator(FD_control, last_image_temp1.shape[0])
+                end_image_temp2 = fractal_generator(FD_control, last_image_temp1.shape[0])
+                end_image_temp3 = fractal_generator(FD_control, last_image_temp1.shape[0])
+
+
+                end_image = generate_rgb_img(end_image_temp1, end_image_temp2, end_image_temp3)
+                next_imgs = delaunay_morphing(last_image, end_image, src_points=None, target_points=None, steps = self.interp_steps, colorspace = 'RGB')
+#                for idx, img in enumerate(next_imgs):
+#                    next_imgs[idx] = contrast(img)
+                stop = time.time()
+                print(stop-start)
+                last_image = end_image
+
+
+class Display(Thread):
+    '''
+    cycle_duration : duration in secs of one pass through next_imgs
+    This thread displays the images in the next_imgs list, and sets to_compute to True
+    to start the generation of the following batch of images
+    '''
+
+    def __init__(self, interp_steps, cycle_duration):
+        Thread.__init__(self)
+        self.waitTime = (int((cycle_duration/interp_steps)*1000)) # REPASSER CA A 1000
+
+    def run(self):
+        global next_imgs, to_compute
+        while True:
+            if next_imgs != []:
+                current_imgs = next_imgs
+                to_compute = True
+                for img in current_imgs:
+                    cv2.imshow('Fractal generator', img)
+                    cv2.waitKey(self.waitTime)
+        #highgui.DestroyWindow('mahMovie)
+
+def renormalize(n, range1, range2):
+    '''
+    Takes a float with range range1 and rescales it into range range2
+    Input :
+    n : float
+    range1 : tuple
+    range2 : tuple
+    Output :
+    float
+    '''
+    delta1 = range1[1] - range1[0]
+    delta2 = range2[1] - range2[0]
+    return (delta2 * (n - range1[0]) / delta1) + range2[0]
+
+def complexity2FD(unused_addr, complexity):
+    global FD_control
+    FD_control = renormalize(complexity, (-2,2), (0.3,0.9))
+    print(FD_control)
+    return FD_control
+
+
+class OSCreceive(Thread):
+    def __init__(self, addr, port, message):
+        Thread.__init__(self)
+        self.addr = addr
+        self.port = port
+        self.message = message
+
+
+    def run(self):
+        global FD_control
+        from pythonosc import dispatcher
+        dispatcher = dispatcher.Dispatcher()
+        dispatcher.map('/svd', complexity2FD)
+        server = osc_server.BlockingOSCUDPServer((self.addr, self.port), dispatcher)
+        print("Serving on {}".format(server.server_address))
+        server.serve_forever()
+
+
+
+
+if __name__ == '__main__':
+    global next_imgs, to_compute, FD_control, size
+    size=1024
+    cycle_duration = 16
+    interp_steps = 6*cycle_duration
+    FD_control = 0.3
+
+    to_compute = True
+    next_imgs = []
+
+    '''
+    dispatcher = dispatcher.Dispatcher()
+    dispatcher.map('/svd', complexity2FD)
+    server = osc_server.ThreadingOSCUDPServer(('localhost', 5055), dispatcher)
+    print("Serving on {}".format(server.server_address))
+    server.serve_forever()
+    '''
+
+    osc = OSCreceive(addr='localhost', port=5055, message='/svd')
+    osc.start()
+
+    gen = Generator(interp_steps=interp_steps)
+    disp = Display(interp_steps=interp_steps, cycle_duration=cycle_duration)
+    gen.start()
+    disp.start()
+
+#size = 1024
+#list_persistence = [0.1, 0.2, 0.3, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90]
---
+++ old-dev/utils.py
@@ -0,0 +1,179 @@
+import numpy as np
+import PIL
+from PIL import Image, ImageEnhance
+
+def generate_rgb_img(img1, img2, img3):
+    img1 = (255*(img1 - np.min(img1))/np.ptp(img1)).astype('uint8')
+    img2 = (255*(img2 - np.min(img2))/np.ptp(img2)).astype('uint8')
+    img3 = (255*(img3 - np.min(img3))/np.ptp(img3)).astype('uint8')
+
+    h = img3
+    s = img2
+    #s = img1-img2*8-img1
+    v = img1
+
+    im_RGB = np.stack((h, s, v), axis=2)
+    img = Image.fromarray(im_RGB, mode='HSV')
+    new_p = np.array(img.convert('RGB'))
+    return new_p
+
+
+def generate_perlin_noise_2d(shape, res):
+    def f(t):
+        return 6*t**5 - 15*t**4 + 10*t**3
+
+    delta = (res[0] / shape[0], res[1] / shape[1])
+    d = (shape[0] // res[0], shape[1] // res[1])
+    grid = np.mgrid[0:res[0]:delta[0],0:res[1]:delta[1]].transpose(1, 2, 0) % 1
+    # Gradients
+    angles = 2*np.pi*np.random.rand(res[0]+1, res[1]+1)
+    gradients = np.dstack((np.cos(angles), np.sin(angles)))
+    g00 = gradients[0:-1,0:-1].repeat(d[0], 0).repeat(d[1], 1)
+    g10 = gradients[1:  ,0:-1].repeat(d[0], 0).repeat(d[1], 1)
+    g01 = gradients[0:-1,1:  ].repeat(d[0], 0).repeat(d[1], 1)
+    g11 = gradients[1:  ,1:  ].repeat(d[0], 0).repeat(d[1], 1)
+    # Ramps
+    n00 = np.sum(np.dstack((grid[:,:,0]  , grid[:,:,1]  )) * g00, 2)
+    n10 = np.sum(np.dstack((grid[:,:,0]-1, grid[:,:,1]  )) * g10, 2)
+    n01 = np.sum(np.dstack((grid[:,:,0]  , grid[:,:,1]-1)) * g01, 2)
+    n11 = np.sum(np.dstack((grid[:,:,0]-1, grid[:,:,1]-1)) * g11, 2)
+    # Interpolation
+    t = f(grid)
+    n0 = n00*(1-t[:,:,0]) + t[:,:,0]*n10
+    n1 = n01*(1-t[:,:,0]) + t[:,:,0]*n11
+    return np.sqrt(2)*((1-t[:,:,1])*n0 + t[:,:,1]*n1)
+
+def generate_fractal_noise_2d(shape, res, octaves=1, persistence=0.5):
+    noise = np.zeros(shape)
+    frequency = 1
+    amplitude = 1
+    for _ in range(octaves):
+        noise += amplitude * generate_perlin_noise_2d(shape, (frequency*res[0], frequency*res[1]))
+        frequency *= 2
+        amplitude *= persistence
+    return noise
+
+
+
+def fractal_generator(persistence, size):
+    perlin = generate_fractal_noise_2d((size,size), (2,2), octaves=8, persistence=persistence)
+    #perlin = (perlin)/np.max(np.max(abs(perlin)))
+    #for i in range(len(perlin)):
+    #    for j in range(len(perlin)):
+    #        if perlin[i][j] >= 0:
+    #            perlin[i][j] = 1
+    #        elif perlin[i][j] < 0:
+    #            perlin[i][j] = -1
+    return perlin
+
+def increments(steps, direction = 0):
+    steps = int(100/steps)
+    a = range(0,100,steps)
+    b = [x for x in a]
+
+    if direction == 1:
+        c = b + [100]
+        b.reverse()
+        d = c + b
+    else:
+        c = b
+        d = c
+    e = [x/100 for x in d]
+    return e
+
+def Interpol(imageA, imageB, steps, direction = 0):
+    inter_matrix = []
+    steps = increments(steps, direction)
+    imageA = Image.fromarray(imageA).convert('L')
+    imageB = Image.fromarray(imageB).convert('L')
+    for k in steps:
+        PIL.Image.blend(imageA, imageB, k)
+        outImage = np.asarray(imageA) * (1.0 - k) + np.asarray(imageB) * k
+        inter_matrix.append(outImage)
+        #final_matrix = np.array(inter_matrix).T
+    return inter_matrix
+
+def contrast(im):
+    im *= 255.0/im.max()
+    image = Image.fromarray(im).convert('L')
+
+    enhancer = ImageEnhance.Contrast(image)
+    factor = 100 #increase contrast
+    im_output = enhancer.enhance(factor)
+    im_output = np.array(im_output)
+    im_out = np.interp(im_output, (im_output.min(), im_output.max()), (-1, +1))
+    return im_out
+
+
+def InterpolMulti(size, list_image, steps, direction = 0):
+    size = size
+    InterpolTemp = np.empty((0, size, size))
+
+    for first, second in zip(list_image, list_image[1:]):
+        InterpolMatrix = Interpol(first, second, steps, direction)
+        InterpolMatrix = np.array(InterpolMatrix)
+        InterpolTemp = np.vstack((InterpolTemp, InterpolMatrix))
+    InterpolMulti = np.array(InterpolTemp)
+    return InterpolMulti
+
+def Generate_list_images(list_persistence, size, n_times=1, save=False):
+    i=0
+    list_temp = []
+    while i < n_times:
+        for x in list_persistence:
+            print(x)
+            image = fractal_generator(x, size)
+            list_temp.append(image)
+        i=i+1
+    list_image = np.array(list_temp)
+    return list_image
+
+def create_anim(img_arr):
+    img = []
+    for i in range(img_arr.shape[2]):
+        im = plt.imshow(img_arr[:,:,i], animated=True, cmap='gray')
+        img.append([im])
+    return img
+
+def rgb2gray(rgb):
+    r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+    gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+    return gray
+
+
+def fractal_dimension(Z, threshold=0.9):
+    # Only for 2d image
+    assert(len(Z.shape) == 2)
+
+    # From https://github.com/rougier/numpy-100 (#87)
+    def boxcount(Z, k):
+        S = np.add.reduceat(
+            np.add.reduceat(Z, np.arange(0, Z.shape[0], k), axis=0),
+                               np.arange(0, Z.shape[1], k), axis=1)
+
+        # We count non-empty (0) and non-full boxes (k*k)
+        return len(np.where((S > 0) & (S < k*k))[0])
+
+    # Transform Z into a binary array
+    Z = (Z < threshold)
+
+    # Minimal dimension of image
+    p = min(Z.shape)
+
+    # Greatest power of 2 less than or equal to p
+    n = 2**np.floor(np.log(p)/np.log(2))
+
+    # Extract the exponent
+    n = int(np.log(n)/np.log(2))
+
+    # Build successive box sizes (from 2**n down to 2**1)
+    sizes = 2**np.arange(n, 1, -1)
+
+    # Actual box counting with decreasing size
+    counts = []
+    for size in sizes:
+        counts.append(boxcount(Z, size))
+
+    # Fit the successive log(sizes) with log (counts)
+    coeffs = np.polyfit(np.log(sizes), np.log(counts), 1)
+    return -coeffs[0]
